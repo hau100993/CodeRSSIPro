6795ab136e70c922a350fbd000582e5dfa270da9
hau100993 <hau100993@gmail.com>
2015-03-17 18:44:43 +0700

    Test: Upload to Github

---
+++ Gittest/git.cpp
@@ -0,0 +1,18 @@
+/*
+ * git.cpp
+ *
+ *  Created on: Mar 17, 2015
+ *      Author: ndhau
+ */
+#include<iostream>
+  using namespace std;
+int main ()
+{
+	int a =123;
+	cout<<"Hello GitHub"<<endl;
+	return 0;
+}
+
+
+
+
