e3fc593b9230c6f5e32d7c9d80792bb2fa666b24
hau100993 <hau100993@gmail.com>
2015-03-17 18:49:13 +0700

    Test

--- Gittest/helloGit.cpp
+++ Gittest/helloGit.cpp
@@ -1,18 +1,8 @@
-/*
- * git.cpp
- *
- *  Created on: Mar 17, 2015
- *      Author: ndhau
- */
 #include<iostream>
   using namespace std;
-int main1 ()
+int main ()
 {
 	int a =123;
 	cout<<"Hello GitHub"<<endl;
 	return 0;
 }
-
-
-
-
